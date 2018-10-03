a33f30120499843cdd767d766f18f21a167e65c1
Gurleen Singh <gurleensinghme@gmail.com>
2018-10-03 00:21:28 -0400

    Reversing the Lists and the Lists within them

---
+++ DeepReverse.py
@@ -0,0 +1,8 @@
+#This code is used to reverse the list and the list present within it.
+
+def deep_reverse(L):
+  L.reverse()			# We reverse the List
+  for l in L:			# We iterate through the List
+    l.reverse()		# We reverse each item which is a list too
+  return L 			# We return it
+print(deep_reverse([[1, 2], [3, 4], [5, 6, 7]]))
